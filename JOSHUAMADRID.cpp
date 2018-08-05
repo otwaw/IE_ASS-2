1b805f311a2d61532a24c1abce1681429957ceed
otwaw <42112340+otwaw@users.noreply.github.com>
2018-08-05 19:24:37 +0800

    Add files via upload

---
+++ JOSHUAMADRID.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    cout<< "How to Tie a Necktie" << endl;
+    cout<< "                    " << endl;
+    cout<< "Step 1. Place tie around your neck" << endl;
+    cout<< "Step 2. Cross the wide end over thinner end" << endl;
+    cout<< "Step 3. Run wide end under tie and pull it across again" << endl;
+    cout<< "Step 4. Pull the wide end through the center" << endl;
+    cout<< "Step 5. Loop through the knot" << endl;
+    cout<< "Step 6. Tighten the knot." << endl;
+    return 0;
+}  
+    
+     
